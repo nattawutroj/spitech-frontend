eb0b0eeccee66d254eff1c1de7c18b7fc11d2035
nattawut rojchanachan <nattawutroj@gmail.com>
2023-11-10 06:13:47 +0700

    :bug: :rocket:

--- src/Routers/Components/NevBar.jsx
+++ src/Routers/Components/NevBar.jsx
@@ -4,8 +4,8 @@ const NevBar = () => {
     return (
         <>
             <ul className="NevBarContainer">
-                <li className="LogoNevBar"><a href='#'><BsMouse /><h1 className='TextNevBar'>SPITECT</h1></a></li>
-                <li className="NevMenu1"><a href='#'><h4 className='LoginText'>ลงชื่อเข้าใช้</h4><h4 className='LoginLogo'><BsPersonCircle /></h4></a></li>
+                <li className="LogoNevBar"><a href='#NewsBottom'><BsMouse /><h1 className='TextNevBar'>SPITECT</h1></a></li>
+                <li className="NevMenu1"><a href='#NewsBottom'><h4 className='LoginText'>ลงชื่อเข้าใช้</h4><h4 className='LoginLogo'><BsPersonCircle /></h4></a></li>
                 <li className="NevMenu"><a href='#NewsBottom'>ดาวน์โหลดเอกสาร</a></li>
                 <li className="NevMenu"><a href='#NewsBottom'>รายชื่อโครงงานพิเศษ</a></li>
             </ul>
